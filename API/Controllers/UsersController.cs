f1c19055b195b051a7f6b996b23b3f9a6b4d117d
amyrosep <75091250+amyrosep@users.noreply.github.com>
2021-06-23 19:03:53 -0400

    End of Section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
@@ -40,5 +41,24 @@ namespace API.Controllers
         {
             return await _userRepository.GetMemberAsync(username);
         }
+
+        [Authorize]
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            //Gives the username from the auth token
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserByUsernameAsync(username);
+
+            //This maps the changes from the request to the user dto
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+            if(await _userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to update user");
+        }
+
     }
 }
---
+++ client/src/app/_interceptors/loading.interceptor.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { BusyService } from '../_services/busy.service';
+import { delay, finalize } from 'rxjs/operators';
+
+@Injectable()
+export class LoadingInterceptor implements HttpInterceptor {
+
+  constructor(private busyService: BusyService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    this.busyService.busy();
+    return next.handle(request).pipe(
+      //It's good to keep this delay for dev purposes. Makes sure spinner is working AND visibility into cacheing
+      delay(1000),
+      finalize(() => {
+        this.busyService.idle();
+      })  
+    );
+  }
+}
