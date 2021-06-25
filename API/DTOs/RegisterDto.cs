5e7cf9fc738b7cba2872bf43f6fba46189942d1f
amyrosep <75091250+amyrosep@users.noreply.github.com>
2021-06-24 20:22:02 -0400

    End of Section 12

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,4 +1,6 @@
+using System;
 using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
 
 namespace API.DTOs
 {
@@ -6,8 +8,15 @@ namespace API.DTOs
     {
         [Required]
         public string Username { get; set; }
+        
         [Required]
         [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
+
+        [Required] public string KnownAs { get; set; }
+        [Required] public string Gender { get; set; }
+        [Required] public DateTime DateOfBirth { get; set; }
+        [Required] public string City { get; set; }
+        [Required] public string Country { get; set; }
     }
 }
